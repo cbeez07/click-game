60aa4226fca2d821f07b45b1eaf914ded287cbfa
Chris B <beesleychc@gmail.com>
2018-06-09 10:04:38 -0500

    Hover effects

--- clickygame/src/components/FriendCard/FriendCard.js
+++ clickygame/src/components/FriendCard/FriendCard.js
@@ -3,7 +3,7 @@ import './FriendCard.css';
 
 const FriendCard = props => (
     <div className="card">
-        <div className="img-container">
+        <div className="img-container hvr-grow-shadow">
             <img onClick={() => props.friendClick(props.id)} className="img-thumbnail" alt={props.name} src={props.image} />
         </div>
         
